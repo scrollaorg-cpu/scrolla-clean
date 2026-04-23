241b839e05628e471352d581b5eeba6961305ae2
Divine <scrolla.org@gmail.com>
2026-04-23 17:53:44 +0100

    Clean rebuild of Scrolla

--- android/app/src/main/kotlin/com/example/scrolla/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.scrolla
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/scrolla/scrolla/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.scrolla.scrolla
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
